fecc5da860526ed42ba8c144a6fa9fffaf2ae28e
Logan Brown <loganbrowncpa@gmail.com>
2019-09-06 21:42:09 -0300

    build script

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+if (process.nextTick.NODE_ENV === "production") {
+  app.use(express.static("loganbrown/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
