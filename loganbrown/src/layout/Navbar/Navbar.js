5c0c9080a7a0debfbe0801ab91b2410cdf39d839
Logan Brown <loganbrowncpa@gmail.com>
2019-09-08 18:20:36 -0300

    added nav links

--- loganbrown/src/layout/Navbar/Navbar.js
+++ loganbrown/src/layout/Navbar/Navbar.js
@@ -1,12 +1,19 @@
 import React from "react";
 
 import "./Navbar.css";
-import strings from "../../constants/strings";
 
 function Navbar() {
   return (
-    <nav className="navbar">
-      <span className="name">{strings.title.name}</span>
+    <nav className="navbar flex-container-row">
+      <section
+        id="link-container"
+        className="nav-links flex-container-row flex-center"
+      >
+        <span className="link fab github fa-github"></span>
+        <span className="link fa education fa-graduation-cap"></span>
+        <span className="link fa experience fa-briefcase"></span>
+        <span className="link fa contact-me fa-envelope"></span>
+      </section>
     </nav>
   );
 }
