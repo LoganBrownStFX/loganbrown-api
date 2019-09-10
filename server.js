e5171f2e5c5ac1ecf5694dc8907c5312077c0988
Logan Brown <loganbrowncpa@gmail.com>
2019-09-09 22:17:19 -0300

    prod fix

--- server.js
+++ server.js
@@ -6,16 +6,14 @@ const github = require("./api/githubRoutes");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use("/api/github", github);
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("loganbrown/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "loganbrown", "build", "index.html"));
   });
-
 }
-
-app.use("/api/github", github);
-
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
