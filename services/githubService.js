b6a8b04c7af6886062cfe9a3a8582d2693e80144
Logan Brown <loganbrowncpa@gmail.com>
2019-09-09 22:24:02 -0300

    production fix

--- services/githubService.js
+++ services/githubService.js
@@ -4,9 +4,9 @@ const axios = require("axios");
 const gitURL = config.CONSTATNS.URLS.GITHUB;
 
 module.exports = class githubService {
-  getRepos = function(userName) {
+  getRepos(userName) {
     axios.get(gitURL + "users/" + userName + "/repos").then(res => {
       return res.data.url;
     });
-  };
+  }
 };
